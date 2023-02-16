7f7a64cb58607d2e584e34410f3bbace74e24489
Gafar Adeniran <gaphcen4better@gmail.com>
2023-02-16 15:56:09 +0100

    sign in page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:biznugget/features/sign_in/sign_in_screen.dart';
 import 'package:biznugget/features/splash_screen/presentation/pages/splash_screen.dart';
 import 'package:biznugget/features/wishlist/presentation/bloc/wishlist_bloc.dart';
 import 'package:biznugget/utils/bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Bloc.observer = MyBlocObserver();
 
   runApp(const MyApp());
@@ -20,11 +26,13 @@ class MyApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider<WishlistBloc>(create: (context) => WishlistBloc()),
+
         /// add your BlocPrvider here
       ],
-      child: MaterialApp(
+      child: const MaterialApp(
         title: 'Biznugget-App',
-        home: SplashScreen(),
+        home: SigninSreen(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
